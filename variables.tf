27cbd5643b1a3ee49c27359d24edeb65fb5d60fa
Jack <jackfperryjr@gmail.com>
2023-12-21 10:06:51 -0600

    Modified input and added terraform steps to workflow.

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "lab_name" { default = "zanarkand" }
 variable "region" { default = "eastus2" }
+variable "users" { default = [] }
 variable "ARM_CLIENT_ID" {}
 variable "ARM_CLIENT_SECRET" {}
 variable "ARM_SUBSCRIPTION_ID" {}
